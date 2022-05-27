c049f9bfc4d13676aafc0ab15dd11032d203af77
Diego <abete.diego@gmail.com>
2022-05-27 19:17:23 +0000

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,40 +1,41 @@
 import unittest
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
 
 from .. import translator
-from translator import Translator
 
 class TestTranslator(unittest.TestCase): 
 
     def test_englishToFrench1(self): 
-        translator = Translator()
-        self.assertEqual(translator.english_to_french(''), '') 
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.english_to_french(''), '') 
 
     def test_englishToFrench2(self): 
-        translator = Translator()
-        self.assertEqual(translator.english_to_french('Hi'), 'Salut') 
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.english_to_french('Hi'), 'Salut') 
 
     def test_englishToFrench3(self): 
-        translator = Translator()
-        self.assertEqual(translator.english_to_french('Thanks'), 'Merci')  
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.english_to_french('Thanks'), 'Merci')  
 
     def test_englishToFrench4(self): 
-        translator = Translator()
-        self.assertEqual(translator.english_to_french('Hello'), 'Bonjour')      
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.english_to_french('Hello'), 'Bonjour')      
 
     def test_frenchToEnglish1(self): 
-        translator = Translator()
-        self.assertEqual(translator.french_to_english(''), '') 
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.french_to_english(''), '') 
 
     def test_frenchToEnglish2(self): 
-        translator = Translator()
-        self.assertEqual(translator.french_to_english('Salut'), "Hi") 
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.french_to_english('Salut'), "Hi") 
     
     def test_frenchToEnglish3(self): 
-        translator = Translator()
-        self.assertEqual(translator.french_to_english('Merci'), 'Thank you') 
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.french_to_english('Merci'), 'Thank you') 
     
     def test_frenchToEnglish4(self): 
-        translator = Translator()
-        self.assertEqual(translator.french_to_english('Bonjour'), 'Hello') 
+        translator_obj = translator.Translator()
+        self.assertEqual(translator_obj.french_to_english('Bonjour'), 'Hello') 
 
 unittest.main()
